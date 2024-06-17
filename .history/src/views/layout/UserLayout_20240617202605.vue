566c183ae7187f53a0aa9cf259fc2c750ef8644f
3403784285-lucky <zplazlily@petalmail.com>
2024-06-17 21:30:44 +0800

    个人信息管理导航栏

--- .history/src/views/layout/UserLayout_20240617202605.vue
+++ .history/src/views/layout/UserLayout_20240617202605.vue
@@ -18,14 +18,14 @@
               class="nav-link tab-test first"
               id="1"
               to="/user/preview"
-              :style="{ backgroundColor: bgColor==1 ? '#afb3af7a' : '#212529' }"
+              :style="{ backgroundColor: (bgColor==1) ? '#afb3af7a' : '#212529' }"
               ><span @click="clickRouter">我的订单</span></router-link
             >
             <router-link
               class="nav-link tab-test"
               id="2"
               to="/user/house"
-              :style="{ backgroundColor: bgColor==2 ? '#afb3af7a' : '#212529' }"
+              :style="{ backgroundColor: (bgColor==2) ? '#afb3af7a' : '#212529' }"
             >
               <span @click="clickRouter">我的房源</span></router-link
             >
@@ -34,7 +34,7 @@
               class="nav-link tab-test"
               id="3"
               to="/user/info"
-              :style="{ backgroundColor: bgColor==3? '#afb3af7a' : '#212529' }"
+              :style="{ backgroundColor: (bgColor ==3)? '#afb3af7a' : '#212529' }"
             >
               <span @click="clickRouter">我的房源</span></router-link
             >
@@ -43,7 +43,7 @@
         </div>
       </div>
       <div class="col-md-10 content-frame">
-        <ul class="nav nav-tabs bg-light choose-nav-one" v-if="flagDisabled==1">
+        <ul class="nav nav-tabs bg-light choose-nav-one" v-if="!flagDisabled">
           <li class="nav-item">
             <div
               class="nav-link kid-list list-test1"
@@ -81,7 +81,7 @@
             </div>
           </li>
         </ul>
-        <ul class="nav nav-tabs bg-light choose-nav-two" v-if="flagDisabled==2">
+        <ul class="nav nav-tabs bg-light choose-nav-two" v-if="flagDisabled">
           <li class="nav-item">
             <div
               class="nav-link kid-list list-test"
@@ -120,11 +120,10 @@ const flagDisabled = ref(false)
 const timeStore = useTimeStore()
 const bgColor = ref(1)
 const clickRouter = (e) => {
-  
-  bgColor.value=e.currentTarget.parentElement.id
-  flagDisabled.value=e.currentTarget.parentElement.id
+  flagDisabled.value=!flagDisabled.value
+  bgColor.value=e.target.id
   console.log(bgColor.value)
-
+  console.log("哈哈哈哈")
 }
 const changeTab = (tabName) => {
   $('.list-test').removeClass('active')
